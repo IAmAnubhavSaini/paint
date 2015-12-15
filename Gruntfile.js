afdb98ffeefcd07341fba7f02efafec7706da1d3
Anubhav Saini <IAmAnubhavSaini@gmail.com>
2015-12-15 10:23:14 +0530

    Closes #3 - installed jasmine.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,23 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    jasmine: {
+      src : 'src/**/*.js',
+      options : {
+        specs : 'test/specs/**/*.js'
+      }
+    },
+
     jshint: {
-      all: ['Gruntfile.js']
+      all: ['Gruntfile.js', 'test/**/*.js']
     }
   });
 
-
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.registerTask('lint', ['jshint']);
-  grunt.registerTask('test', ['jshint']);
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('test', ['jshint', 'jasmine']);
+  grunt.registerTask('default', ['jshint', 'jasmine']);
 
 };
---
+++ test/specs/humor-jasmine.spec.js
@@ -0,0 +1,5 @@
+describe("Just to please issue #3", function(){
+  it("tells truth", function(){
+    expect(2+4).toBe(6);
+  });
+});
